486fe60478b12dacd7d29847258e164a2dd3723a
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-22 18:45:14 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,15 @@
 class Team 
   
   attr_accessor :name, :motto
+  @@all = []
   
   def initialize(name, motto)
     @name = name 
     @motto = motto
+    @@all << self 
   end 
   
+  def self.all 
+    @@all 
+  end 
 end 
