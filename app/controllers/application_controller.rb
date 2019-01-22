8fac1b51537fefaa8223bdbde39781473781bcc2
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-22 18:37:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
-require 'sinatra/base'
 
+
+require 'sinatra/base'
+require 'pry'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
+    get '/' do 
+      erb :createteam
+    end
+    
+    post '/teams' do 
+      binding.pry
+      
+      erb :team 
+    end 
 end
