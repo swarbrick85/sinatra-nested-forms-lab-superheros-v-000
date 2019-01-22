c216632df8c02ab3d3ea8b6c679c6c3e7d69f138
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-22 18:52:11 +0000

    Automatically backed up by Learn

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,10 +3,10 @@ class Member
   attr_accessor :name, :power, :bio 
   @@all = []
   
-  def initialize(name, power, bio)
-    @name = name 
-    @power = power 
-    @bio = bio 
+  def initialize(args)
+    @name = args[:name] 
+    @power = args[:power] 
+    @bio = args[:bio] 
     @@all << self 
   end 
   
