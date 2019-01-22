809e9e8464963d44e1e47e69e0b9f0ac449aa4f3
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-22 18:46:10 +0000

    Automatically backed up by Learn

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,11 +1,20 @@
 class Member 
   
   attr_accessor :name, :power, :bio 
+  @@all = []
   
   def initialize(name, power, bio)
     @name = name 
     @power = power 
     @bio = bio 
+    @@all << self 
   end 
   
+  def self.all 
+    @@all 
+  end 
+  
+  def self.clear 
+    @@all = []
+  end 
 end 
